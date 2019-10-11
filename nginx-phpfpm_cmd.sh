43e99fd099050f37ca1c41504e04fc0fa4af6b90
admin <34087902+wozhangkun@users.noreply.github.com>
2019-10-11 11:59:59 +0800

    Update nginx-phpfpm_cmd.sh

--- nginx-phpfpm_cmd.sh
+++ nginx-phpfpm_cmd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-/usr/local/php/sbin/php-fpm --nodaemonize --fpm-config /usr/local/php/etc/php-fpm.conf && nginx
+/usr/local/php/sbin/php-fpm --nodaemonize --fpm-config /usr/local/php/etc/php-fpm.conf && /usr/sbin/nginx
