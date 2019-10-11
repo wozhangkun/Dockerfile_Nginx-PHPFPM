1192c5cea3faba5f073b0341e2e74508de5e9db4
admin <34087902+wozhangkun@users.noreply.github.com>
2019-10-11 17:21:05 +0800

    Update nginx-phpfpm_ENTRYPOINT.sh

--- nginx-phpfpm_ENTRYPOINT.sh
+++ nginx-phpfpm_ENTRYPOINT.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-/usr/sbin/nginx -g "daemon off;"
+#多个进程需要将第一个进程设置为daemon运行方式（nginx默认为daemon运行方式），否则第二个进程不会运行。
+/usr/sbin/nginx -g "daemon on;"
 /usr/local/php/sbin/php-fpm --nodaemonize --fpm-config /usr/local/php/etc/php-fpm.conf
